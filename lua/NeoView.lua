6924cbde3cf71fe8a0bb8f52d4ec1761ce099c57
Samuel Jackson <dsiq3g@gmail.com>
2024-09-26 22:02:10 -0700

    Improve saving functionality
    This fixes a few issues with the saving and loading functionality.
    Previously, when a file loaded, this plugin was pretty much only relying
    on the functionality of the mkview function and was not actually loading
    the position from the file. Now, I have more checks in place to make
    sure we know ahead of time how the cursor needs to be placed while still
    maintaining the file's fold state.

--- lua/NeoView.lua
+++ lua/NeoView.lua
@@ -10,6 +10,14 @@
 |__/  \__/ \_______/ \______/     \_/    |__/ \_______/ \_____/\___/
 
 --]]
+
+-- TODO
+-- 1. Make an acual config to set things up.
+-- 1.1. Directories
+-- 1.2. Disabled filetypes
+-- 1.3. Disable opening fold under cursor at startup
+-- 2. Save views settings and wrap it so the view gets changed when used by neoview but then set back to the user defined settings.
+-- 3. Experiment if this can be done with just `mkview` and not need the extra cursor position saving.
 local NeoView = {}
 -- Save stuff in here for now I guess
 local user_options = {}
@@ -32,29 +40,23 @@ NeoView.setup = function(opts)
 
 	vim.cmd('silent! set viewdir=' .. VIEWS_DIR)
 
+	vim.opt.viewoptions = { "folds" }
+
+	local group = vim.api.nvim_create_augroup('NeoView', { clear = true })
+
 	vim.api.nvim_create_autocmd('BufWinEnter', {
-		group = vim.api.nvim_create_augroup('NeoView', { clear = true }),
+		group = group,
 		callback = function()
 			pcall(function()
-				-- This is an attempt to allow functions that take you
-				-- to other files not have their cursor positions moved
-				-- from the spot they're taking you to.
-				-- ex. telescope grep
-				local r, c = unpack(vim.api.nvim_win_get_cursor(0))
-				-- check for the start of the buffer. using 2 is arbitrary since I
-				-- THINK the original cursor position is (1,0) but I can't guarentee that'll
-				-- always be the case.
-				if (r < 2 and c < 2) then
-					NeoView.restore_view()
-				end
+				vim.schedule(NeoView.restore_cursor_position)
 			end)
 		end,
 	})
 
 	vim.api.nvim_create_autocmd({ 'BufUnload', 'BufWinLeave' }, {
-		group = 'NeoView',
+		group = group,
 		callback = function()
-			pcall(function() NeoView.save_view() end)
+			pcall(function() NeoView.save_folds() end)
 			pcall(function() NeoView.save_cursor_position() end)
 		end,
 	})
@@ -62,22 +64,41 @@ NeoView.setup = function(opts)
 	vim.api.nvim_create_user_command('ClearNeoView', require('NeoView').clear_neoview, {})
 end
 
-function NeoView.save_view()
+function NeoView.save_folds()
 	if NeoView.valid_buffer() then
 		vim.cmd('silent! mkview!')
 	end
 end
 
-function NeoView.restore_view()
+-- If the code at the cursor is folded, unfold it. This may not always be desired,
+-- but I feel generally it's better to see the code at the point of entry
+-- TODO find a way to only upen the folds under cursor, not all folds in the area.
+-- This can happen when the cursor is in the middle of many nested folds. ex:
+--	function()
+--		if() then
+--			...
+--		end
+--		if() then
+--			... <- Cursor here
+--		end
+--		if() then
+--			...
+--		end
+--	end
+-- In the above example, if everything is closed, we will open ALL the if statements. Don't want that.
+local function open_current_fold()
+	vim.cmd('silent! exe "normal! zO"')
+end
+
+function NeoView.restore_folds()
 	if NeoView.valid_buffer() then
 		vim.cmd('silent! loadview')
-		vim.schedule(NeoView.restore_cursor_position)
 	end
 end
 
-function NeoView.notify_neoview()
+function NeoView.notify_neoview(s)
 	local timer = vim.loop.new_timer()
-	vim.notify('NeoView Data Cleared')
+	vim.notify(s)
 
 	if timer then
 		timer:start(3000, 0, vim.schedule_wrap(function()
@@ -95,12 +116,34 @@ function NeoView.clear_neoview()
 	if vim.fn.filereadable(CURSOR_FILE) == 1 then
 		vim.fn.delete(CURSOR_FILE)
 	end
-	NeoView.notify_neoview()
+	NeoView.notify_neoview('NeoView Data Cleared')
 end
 
 function NeoView.restore_cursor_position()
 	if not NeoView.valid_buffer() then return end
 
+	-- Get Starting cursor position. If you were brought to the
+	-- current file via a link or reference, then the cursor will
+	-- be on the relevant line and not on the first line. In that case,
+	-- restore the folds and then set the cursor to the original position.
+	--
+	-- If the cursor is at the start of the file, then load the cursor's saved
+	-- location if one exists.
+	local r, _ = unpack(vim.api.nvim_win_get_cursor(0)) -- unused char character
+
+	-- Case where cursor is already on something
+	if r > 1 then
+		-- Re-getting cursor's position since this is in the format needed below.
+		local cur_pos = vim.fn.getpos('.')
+		NeoView.restore_folds()
+		vim.fn.setpos('.', cur_pos)
+		open_current_fold()
+		return
+	end
+
+
+	-- Case where we need to load cursor's position from file
+	NeoView.restore_folds()
 	if vim.fn.filereadable(CURSOR_FILE) == 1 then
 		local file_content = table.concat(vim.fn.readfile(CURSOR_FILE))
 		local cursor_data_all = vim.fn.json_decode(file_content)
@@ -110,15 +153,16 @@ function NeoView.restore_cursor_position()
 
 		if cursor_data then
 			vim.fn.setpos('.', cursor_data.cursor)
+			open_current_fold()
 		end
 	end
 end
 
 function NeoView.save_cursor_position()
+	if not NeoView.valid_buffer() then return end
+
 	local file_path_key = vim.fn.expand('%:p')
 	local cursor_position = vim.fn.getpos('.')
-
-	if not NeoView.valid_buffer() then return end
 
 	local cursor_data_all = {}
 	if vim.fn.filereadable(CURSOR_FILE) == 1 then
@@ -141,6 +185,8 @@ function NeoView.valid_buffer()
 	-- TODO add this to the opts
 	local filetype = vim.bo.filetype
 	local disabled_filetypes = user_options.disabled_filetypes or { 'NeogitCommitMessage' }
+	-- If the code at the cursor is folded, unfold it. This may not always be desired,
+	-- but I feel generally it's better to see the code at the point of entry
 	if vim.tbl_contains(disabled_filetypes, filetype) then return false end
 
 	return true
